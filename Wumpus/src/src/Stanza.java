063f23e914f41b4f8cf2dd292329062f46dc419c
martinabalossino1-crypto <martinabalossino1@gmail.com>
2026-06-05 15:40:57 +0200

    Update Stanza.java

--- Wumpus/src/src/Stanza.java
+++ Wumpus/src/src/Stanza.java
@@ -6,6 +6,52 @@ public class Stanza {
 	
 	int id;
 	public enum Stato {POZZO,PIPISTRELLI,TUMAEROS,WUMPUS,GIOCATORE};
+	ArrayList<Stato> stato;
+	
+	public Stanza (int id) {
+		this.stato = new ArrayList<>();
+	}
+	
+	public int cercaStato (Stato stato) {
+		for (Stato s : this.stato) {
+			if (s==stato) return 1;
+		}
+		return 0;
+    }
+	
+	public void removeStato(Stato stato) {
+		this.stato.remove(stato);
+	}
+	
+	public int setStatoIfEmpty(Stato stato) {
+		if(this.stato.isEmpty()) {
+			this.stato.add(stato);
+			return 1;
+		}
+		return 0;
+	}
+	
+	public void setStato(Stato stato) {
+		this.stato.add(stato);
+	}
+	
+	public ArrayList<Stato> getArrayStato(){
+		return this.stato;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+
+}
+package src;
+
+import java.util.ArrayList;
+
+public class Stanza {
+	
+	int id;
+	public enum Stato {POZZO,PIPISTRELLI,TUMAEROS,WUMPUS,GIOCATORE};
 	//public int occupata;
 	ArrayList<Stato> stato;
 	
