fec2db264f1511fbbc78308c5ee34ede120dfdbf
martinabalossino1-crypto <martinabalossino1@gmail.com>
2026-06-09 17:57:58 +0200

    Update Grotta.java

--- Wumpus/src/src/Grotta.java
+++ Wumpus/src/src/Grotta.java
@@ -2,9 +2,14 @@ package src;
 
 public class Grotta {
 	
-	static int[] vTunnels;
-	static Stanza[] vStanze;
+	public static int[] vTunnels;
+	public static Stanza[] vStanze;
 	
+	/**
+	 * Costruttore
+	 * 
+	 * @return un oggetto di tipo Grotta
+	 */
 	public Grotta() {
 		vStanze = new Stanza[20];
 		for (int i=0;i<=19;i++) {
@@ -12,42 +17,32 @@ public class Grotta {
 		}
 	}
 	
+	/**
+	 * Ritorna la stanza presente all'indice passato in input del vettore vStanze
+	 * 
+	 * @param stanza Id stanza, che corrisponde anche alla sua posizione nel vettore vStanze
+	 * @return la stanza indicata
+	 */
 	public Stanza getStanza(int stanza) {
 		return vStanze[stanza];
 	}
 	
+	/**
+	 * Ritorna l'array che indica quali sono le stanze adiacenti ad ogni stanza della grotta
+	 * 
+	 * @return Array dei tunnel
+	 */
 	public int[] getTunnel() {
 		return vTunnels;
 	}
 	
-	public static int percorsoPossibile(int stanza1, int stanza2) {
-		if(vTunnels[stanza1*3]==stanza2||vTunnels[stanza1*3+1]==stanza2||vTunnels[stanza1*3+2]==stanza2) return 1;
-		return 0;
-	}
-	
-}
-package src;
-
-public class Grotta {
-	
-	static int[] vTunnels = {/*0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,*/1,4,7,0,2,9,1,3,11,2,4,13,0,3,5,4,6,14,5,7,16,0,6,8,7,9,17,1,8,10,9,11,18,2,10,12,11,13,19,3,12,14,5,13,15,14,16,19,6,15,17,8,16,18,10,17,19,12,15,18};
-	Stanza[] vStanze;
-	
-	public Grotta() {
-		this.vStanze = new Stanza[20];
-		for (int i=0;i<=19;i++) {
-				this.vStanze[i] = new Stanza(i);
-		}
-	}
-	
-	public Stanza getStanza(int stanza) {
-		return this.vStanze[stanza];
-	}
-	
-	public int[] getTunnel() {
-		return vTunnels;
-	}
-	
+	/**
+	 * Indica se una certa stanza è adiacente ad un'altra andando a leggere quali stanze sono adiacenti alla prima dal vettore vTunnel
+	 * 
+	 * @param stanza1 stanza di partenza
+	 * @param stanza2 stanza in cui si vuole andare
+	 * @return 1 se le due stanze sono adiacenti, 0 altrimenti
+	 */
 	public static int percorsoPossibile(int stanza1, int stanza2) {
 		if(vTunnels[stanza1*3]==stanza2||vTunnels[stanza1*3+1]==stanza2||vTunnels[stanza1*3+2]==stanza2) return 1;
 		return 0;
