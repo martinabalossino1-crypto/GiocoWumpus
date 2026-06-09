f0daf2a08fbe31610cca90e1679b13b957d4b4cb
martinabalossino1-crypto <martinabalossino1@gmail.com>
2026-06-09 17:58:46 +0200

    Update Mobius.java

--- Wumpus/src/src/Mobius.java
+++ Wumpus/src/src/Mobius.java
@@ -2,9 +2,23 @@ package src;
 
 public class Mobius extends Grotta {
 	
+	/**
+	 * Costruttore
+	 * @return Oggetto Mobius
+	 */
 	public Mobius() {
 		super();
 		Grotta.vTunnels = new int[] {1,10,19,0,2,11,1,3,12,2,4,13,3,5,14,4,6,15,5,7,16,6,8,17,7,9,18,8,10,19,0,11,9,10,1,12,11,2,13,2,12,14,13,4,15,5,14,16,15,6,17,16,7,18,8,17,19,18,9,0};
+	}
+	
+	/**
+	 * Ritorna la visualizzazione del nastro di Mobius
+	 * 
+	 * @return Stringa contenente la rappresentazione della grotta
+	 */	
+	public String toString() {
+		return "			2 ----- 3		"
+			+ "		1";
 	}
 	
 }
