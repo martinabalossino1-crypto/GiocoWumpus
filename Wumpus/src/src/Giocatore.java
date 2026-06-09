61b6441f85f8f39550cd4abda5ff643eca2383d5
martinabalossino1-crypto <martinabalossino1@gmail.com>
2026-06-09 17:53:33 +0200

    Update Giocatore.java

--- Wumpus/src/src/Giocatore.java
+++ Wumpus/src/src/Giocatore.java
@@ -6,55 +6,52 @@ public class Giocatore {
 	int frecce;
 	int stanza;
 	
+	/**
+	 * Costruttore
+	 * 
+	 * @param numFrecce Numero di frecce con cui il giocatore vuole iniziare a giocare
+	 * @param stanza Prima stanza in cui appare il giocatore
+	 * @return Oggetto Giocatore
+	 */	
 	public Giocatore(int numFrecce, int stanza) {
 		this.frecce=numFrecce;
 		this.stanza=stanza;
 	}
 	
-	public int getFrecce() {
-		return this.frecce;
-	}
-	
-	public int getStanza() {
-		return this.stanza;
-	}
-	
-	public void setStanza(int stanza) {
-		this.stanza=stanza;
-	}
-	
+	/**
+	 * Sottrae una freccia da quelle possedute dal giocatore
+	 * 
+	 * @return Descrizione del valore restituito.
+	 */
 	public void eliminaFreccia() {
 		this.frecce--;
 	}
 	
-}
-package src;
-
-
-public class Giocatore {
-	
-	int frecce;
-	int stanza;
-	
-	public Giocatore(int numFrecce, int stanza) {
-		this.frecce=numFrecce;
-		this.stanza=stanza;
-	}
-	
-	public int getFrecce() {
-		return this.frecce;
-	}
-	
-	public int getStanza() {
-		return this.stanza;
-	}
-	
+	/**
+	 * Setta il numero della stanza in cui si trova il giocatore
+	 * 
+	 * @param stanza Id della stanza da settare
+	 */
 	public void setStanza(int stanza) {
 		this.stanza=stanza;
 	}
 	
-	public void eliminaFreccia() {
-		this.frecce--;
+	/**
+	 * Ritorna il numero di frecce possedute dal giocatore
+	 * 
+	 * @return valore contenuto nell'attributo "frecce"
+	 */
+	public int getFrecce() {
+		return this.frecce;
 	}
 	
+	/**
+	 * Ritorna l'id della stanza in cui si trova il giocatore
+	 * 
+	 * @return Id stanza giocatore
+	 */
+	public int getStanza() {
+		return this.stanza;
+	}
+		
 }
