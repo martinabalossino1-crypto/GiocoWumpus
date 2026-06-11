0a38dedf0bee62cbee07449f4825dabf333db643
martinabalossino1-crypto <martinabalossino1@gmail.com>
2026-06-11 17:03:43 +0200

    Update WumpusGameException.java

--- Wumpus/src/src/WumpusGameException.java
+++ Wumpus/src/src/WumpusGameException.java
@@ -3,6 +3,53 @@ package src;
 
 public class WumpusGameException extends Exception{
 
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7986241308775561076L;
+
+	/**
+	 * Costruttore
+	 * @return Ritorna l'oggetto WumpusGameException
+	 */	
+	public WumpusGameException() {
+		super();
+	}
+	
+	/**
+	 * Costruttore
+	 * @param Messaggio Si tratta della stringa da stampare nel caso venga generata un'eccezione
+	 * @return Ritorna l'oggetto WumpusGameException
+	 */	
+	public WumpusGameException(String messaggio) {
+		super(messaggio);
+	} 
+	
+	/**
+	 * Costruttore
+	 * @param Messaggio Si tratta della stringa da stampare nel caso venga generata un'eccezione
+	 * @param thr E' l'oggetto Throwable che indica l'anomalia che si è verificata
+	 * @return Ritorna l'oggetto WumpusGameException
+	 */	
+	public WumpusGameException(String messaggio, Throwable thr) {
+		super(messaggio, thr);
+	}
+	
+	/**
+	 * Costruttore
+	 * @param thr E' l'oggetto Throwable che indica l'anomalia che si è verificata
+	 * @return Ritorna l'oggetto WumpusGameException
+	 */	
+	public WumpusGameException(Throwable thr) {
+		super(thr);
+	}
+}
+package src;
+
+
+public class WumpusGameException extends Exception{
+
 	/**
 	 * 
 	 */
